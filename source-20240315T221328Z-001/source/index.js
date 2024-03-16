897b92335eb7035bd0c092f84e3fcc3eccf2627c
famil <maxgaudreau9@gmail.com>
2024-03-15 20:08:39 -0400

    asdf

--- source-20240315T221328Z-001/source/index.js
+++ source-20240315T221328Z-001/source/index.js
@@ -2,11 +2,13 @@ const chatForm = get("form");
 const chatInput = get("input");
 const chatBox = get("main");
 
-const introText = 'Hello, I am a medical bot that can help you determine the severity and actions to take reagarding a medical problem.'
-const medInfo = 'Please provide me the following informations regarding your medical situation:Age, Sex, Pre-existing conditions as well as current condition'
+const introText =
+  "Hello, I am a medical bot that can help you determine the severity and actions to take reagarding a medical problem.";
+const medInfo =
+  "Please provide me the following informations regarding your medical situation:Age, Sex, Pre-existing conditions as well as current condition";
 
-appendMessage('bot', introText)
-appendMessage('bot', medInfo)
+appendMessage("bot", introText);
+appendMessage("bot", medInfo);
 
 chatForm.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -14,10 +16,19 @@ chatForm.addEventListener("submit", async (event) => {
   if (!text) return;
 
   query({
-    inputs: 'Question:' + text + 'How critical is my condition and whats actions should i take?' + 'Answer:',
-    parameters: {},
+    inputs:
+      "Context: " +
+      "You should either Ask me more infos on myself to help diagnose my condition, give recommendations on medications, give a diagnosis, lead me towards the right specialist instead of just doctor if necessary. I want a complete diagnosis so ask questions as much as you want" +
+      " " +
+      "Question:" +
+      text +
+      "Answer:",
+    // inputs: 'Question:' + text + 'How critical is my condition and whats actions should i take?' + 'Answer:',
+    parameters: {
+      max_new_tokens: 150,
+    },
   }).then((response) => {
-    appendMessage("bot", response[0].generated_text)
+    appendMessage("bot", response[0].generated_text);
   });
 
   appendMessage("user", text);
