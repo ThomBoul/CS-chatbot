3c0fbc46be8a44fb6476915116f358e63f2c633c
Thomas Boulard <thomas@crewdle.com>
2024-03-15 20:33:07 -0400

    masmd

--- source-20240315T221328Z-001/source/index.js
+++ source-20240315T221328Z-001/source/index.js
@@ -1,35 +1,43 @@
-const chatForm = get("form");
-const chatInput = get("input");
-const chatBox = get("main");
+const chatForm = get('form');
+const chatInput = get('input');
+const chatBox = get('main');
 
 const introText = 'Hello, I am a medical bot that can help you determine the severity and actions to take reagarding a medical problem.'
-const medInfo = 'Please provide me the following informations regarding your medical situation:Age, Sex, Pre-existing conditions as well as current condition'
+const medInfo = 'Please provide me the following informations regarding your medical situation: Age, Sex, Pre-existing conditions as well as current condition'
+
+let first = false
+let totalText = ''
 
 appendMessage('bot', introText)
 appendMessage('bot', medInfo)
 
-chatForm.addEventListener("submit", async (event) => {
+chatForm.addEventListener('submit', async (event) => {
   event.preventDefault();
   const text = chatInput.value;
   if (!text) return;
+  totalText += text
+  let inputText =  'Comment:' + totalText + '. Answer:'
 
   query({
-    inputs: 'Question:' + text + 'How critical is my condition and whats actions should i take?' + 'Answer:',
-    parameters: {},
+    inputs: inputText,
+    parameters: {
+      'max_new_tokens': 200,
+    },
   }).then((response) => {
-    appendMessage("bot", response[0].generated_text)
+    totalText += response
+    appendMessage('bot', response[0].generated_text)
   });
 
-  appendMessage("user", text);
-  chatInput.value = "";
+  appendMessage('user', text);
+  chatInput.value = '';
 });
 
 function appendMessage(side, text) {
   const bubble = `
-    <div class="msg -${side}">
-        <div class="bubble">${text}</div>
+    <div class='msg -${side}'>
+        <div class='bubble'>${text}</div>
     </div>`;
-  chatBox.insertAdjacentHTML("beforeend", bubble);
+  chatBox.insertAdjacentHTML('beforeend', bubble);
   chatBox.scrollTop += 500;
 }
 
@@ -40,13 +48,13 @@ function get(selector, root = document) {
 
 async function query(data) {
   const response = await fetch(
-    "https://xevhza5rhd1jhkq8.us-east-1.aws.endpoints.huggingface.cloud",
+    'https://xevhza5rhd1jhkq8.us-east-1.aws.endpoints.huggingface.cloud',
     {
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify(data),
     }
   );
